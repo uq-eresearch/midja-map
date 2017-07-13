512aa706b048d6c1390d08b7f02f2257858061c5
Tim Dettrick <t.dettrick@uq.edu.au>
2017-07-13 13:32:46 +1000

    Handle Census 2016 datapack import
    All attributes present in the 2016 census are now importable. The
    primary change has been to use short headers, as the 2016 datapacks only
    use this CSV header form.
    
    Due to the difficulties involved with short headers, the long form has
    been left for education attributes that are not available yet for 2016.

--- scripts/abs-csv-dump.js
+++ scripts/abs-csv-dump.js
@@ -41,17 +41,17 @@ const matchers = [
     const params =
       R.merge(
         extract(
-          /(\d{4})Census_I04_AUST_(\w+)_long/,
+          /(\d{4})Census_I04_AUST?_([^_\.]+)(?:_short)?\.csv$/,
           'year',
           'regionType')(filename),
         extract(
-          /median_total_household_income_weekly_(.+)$/i,
+          /Med?_hhd?_inc?_wk_(.+)$/,
           'suffix')(field));
     return orEmpty(
       hasKeys('year', 'regionType', 'suffix'),
       chainApplyToAll(
         orEmpty(
-          R.pipe(R.prop('suffix'), R.test(/total$/i)),
+          R.pipe(R.prop('suffix'), R.test(/Tot$/)),
           params => [{
             regionType: params.regionType.toLowerCase() + '_' + params.year,
             attribute: {
@@ -64,7 +64,7 @@ const matchers = [
           }]
         ),
         orEmpty(
-          R.pipe(R.prop('suffix'), R.test(/with_indigenous_persons$/i)),
+          R.pipe(R.prop('suffix'), R.test(/In_ps_hh_In_ps$/)),
           params => [{
             regionType: params.regionType.toLowerCase() + '_' + params.year,
             attribute: {
@@ -83,11 +83,11 @@ const matchers = [
     const params =
       R.merge(
         extract(
-          /(\d{4})Census_I06_AUST_(\w+)_long/,
+          /(\d{4})Census_I06_AUST?_([^_\.]+)(?:_short)?\.csv$/,
           'year',
           'regionType')(filename),
         extract(
-          /^persons_(year_12_or_equivalent|total)_(indigenous|total)$/i,
+          /^P_(Y12_equivalent|Tot)_(Indigenous|Tot)$/i,
           'prefix',
           'suffix')(field));
     return orEmpty(
@@ -95,8 +95,8 @@ const matchers = [
       chainApplyToAll(
         orEmpty(
           R.where({
-            'prefix': R.test(/^year_12/i),
-            'suffix': R.test(/^indigenous$/i)
+            'prefix': R.equals('Y12_equivalent'),
+            'suffix': R.equals('Indigenous')
           }),
           params => [{
             regionType: params.regionType.toLowerCase() + '_' + params.year,
@@ -111,8 +111,8 @@ const matchers = [
         ),
         orEmpty(
           R.where({
-            'prefix': R.test(/^total$/i),
-            'suffix': R.test(/^indigenous$/i)
+            'prefix': R.equals('Tot'),
+            'suffix': R.equals('Indigenous')
           }),
           params => [{
             regionType: params.regionType.toLowerCase() + '_' + params.year,
@@ -127,8 +127,8 @@ const matchers = [
         ),
         orEmpty(
           R.where({
-            'prefix': R.test(/^year_12/i),
-            'suffix': R.test(/^total$/i)
+            'prefix': R.equals('Y12_equivalent'),
+            'suffix': R.equals('Tot')
           }),
           params => [{
             regionType: params.regionType.toLowerCase() + '_' + params.year,
@@ -143,8 +143,8 @@ const matchers = [
         ),
         orEmpty(
           R.where({
-            'prefix': R.test(/^total$/i),
-            'suffix': R.test(/^total$/i)
+            'prefix': R.equals('Tot'),
+            'suffix': R.equals('Tot')
           }),
           params => [{
             regionType: params.regionType.toLowerCase() + '_' + params.year,
@@ -164,15 +164,16 @@ const matchers = [
     const params =
       R.evolve(
         {
-          suffix: v => /indigenous/i.test(v) ? 'indigenous' : 'all'
+          low: v => /^HI/.test(v) ? v.slice(2) : v,
+          suffix: v => v == 'Tot_hhlds' ? 'all' : 'indigenous'
         },
         R.merge(
           extract(
-            /(\d{4})Census_I13_AUST_(\w+)_long/,
+            /(\d{4})Census_I13_AUST?_([^_\.]+)(?:_short)?\.csv$/,
             'year',
             'regionType')(filename),
           extract(
-            /^(\d+|negative|partial|all)_(\d+|nil_income|or_more|income.*_stated)_(households_with_indigenous_persons|total_households)$/i,
+            /^(HI\d+|Neg|part(?:ial)?|all)_(\d+|Nil_inc(?:ome)?|more|inc(?:ome)?(?:_st(?:ated)?|_ns)?)_(hhd?_with_Ind(?:ig)?_psns?|Tot_hhlds)$/i,
             'low',
             'high',
             'suffix')(field)
@@ -183,12 +184,12 @@ const matchers = [
       chainApplyToAll(
         orEmpty(
           R.where({
-            'low': R.test(/^partial$/i)
+            'low': R.test(/^part(ial)?$/i)
           }),
           params => [{
             regionType: params.regionType.toLowerCase() + '_' + params.year,
             attribute: {
-              "name": `census${params.year}_partially_unstated_weekly_household_income_${params.suffix}`,
+              "name": `census${params.year}_partially_unstated_weekly_household_income_${params.suffix}`.toLowerCase(),
               "description": `Partially-unstated weekly household income - ${params.suffix} households (Census ${params.year})`,
               "type": "number",
               "format": integerFormat,
@@ -228,7 +229,7 @@ const matchers = [
         ),
         orEmpty(
           R.where({
-            'high': R.test(/more$/)
+            'high': R.equals('more')
           }),
           params => [{
             regionType: params.regionType.toLowerCase() + '_' + params.year,
@@ -267,7 +268,7 @@ const matchers = [
         },
         R.merge(
           extract(
-            /(\d{4})Census_I15._AUST_(\w+)_long/i,
+            /(\d{4})Census_I15._AUST?_(\w+)_long/i,
             'year',
             'regionType')(filename),
           extract(
@@ -296,15 +297,15 @@ const matchers = [
     const params =
       R.evolve(
         {
-          status: v => /indigenous/i.test(v) ? 'indigenous' : 'all'
+          status: v => v == 'Tot' ? 'all' : 'indigenous'
         },
         R.merge(
           extract(
-            /(\d{4})Census_I04_AUST_(\w+)_long/i,
+            /(\d{4})Census_I04_AUST?_([^_\.]+)(?:_short)?\.csv$/i,
             'year',
             'regionType')(filename),
           extract(
-            /^(average_number_of_persons_per_bedroom|average_household_size)_(indigenous|total)/i,
+            /^(Av_hh_si|Avg_household_size)_(In_ps_hh_wi_Ind_psns|Tot)/,
             'statistic',
             'status')(field)
         )
@@ -314,8 +315,8 @@ const matchers = [
       params => [{
         regionType: params.regionType.toLowerCase() + '_' + params.year,
         attribute: {
-          "name": `census${params.year}_${params.statistic}_${params.status}`.toLowerCase(),
-          "description": `${params.statistic.replace(/_/g,' ')} - ${params.status} (Census ${params.year})`,
+          "name": `census${params.year}_average_household_size_${params.status}`.toLowerCase(),
+          "description": `Average household size - ${params.status} (Census ${params.year})`,
           "type": "number",
           "format": {
             "maximumFractionDigits": 1
@@ -329,15 +330,59 @@ const matchers = [
     const params =
       R.evolve(
         {
-          high: v => v.replace(/_?years_?/i, '')
+          status: v => v == 'Tot' ? 'all' : 'indigenous'
         },
         R.merge(
           extract(
-            /(\d{4})Census_I03._AUST_(ILOC)_long/i,
+            /(\d{4})Census_I04_AUST?_([^_\.]+)(?:_short)?\.csv$/i,
             'year',
             'regionType')(filename),
           extract(
-            /^age_years_(\d+)_(\d+_years|years_and_over)_(indigenous|total)_(males|females|persons)$/i,
+            /^(Av_N_p_pe_be|Avg_Nu_psns_per_bed)_(In_ps_hh_wi?_In_p|Tot)/i,
+            'statistic',
+            'status')(field)
+        )
+      )
+    return orEmpty(
+      hasKeys('year', 'regionType', 'statistic', 'status'),
+      params => [{
+        regionType: params.regionType.toLowerCase() + '_' + params.year,
+        attribute: {
+          "name": `census${params.year}_average_number_of_persons_per_bedroom_${params.status}`.toLowerCase(),
+          "description": `Average number of persons per bedroom - ${params.status} (Census ${params.year})`,
+          "type": "number",
+          "format": {
+            "maximumFractionDigits": 1
+          },
+          "source": sourceDetails(filename, field)
+        }
+      }]
+    )(params)
+  },
+  (filename, field) => {
+    const params =
+      R.evolve(
+        {
+          gender: v => {
+            switch (v) {
+              case 'M': return 'Males'
+              case 'F': return 'Females'
+              case 'P': return 'Persons'
+              default:  return null
+            }
+          },
+          high: v => {
+            return v.replace('_', '')
+          },
+          status: v => v == 'Tot' ? 'Total' : 'Indigenous'
+        },
+        R.merge(
+          extract(
+            /(\d{4})Census_I03._AUST?_(ILOC)(?:_short)?\.csv$/i,
+            'year',
+            'regionType')(filename),
+          extract(
+            /^Age_(\d+)(_\d+|_?over)_(Indig|Tot)_?(M|F|P)$/i,
             'low',
             'high',
             'status',
@@ -364,7 +409,7 @@ const matchers = [
         ),
         orEmpty(
           R.where({
-            'high': R.equals("and_over")
+            'high': R.equals("over")
           }),
           params => [{
             regionType: params.regionType.toLowerCase() + '_' + params.year,
@@ -382,15 +427,28 @@ const matchers = [
   },
   (filename, field) => {
     const params =
-      R.merge(
-        extract(
-          /(\d{4})Census_I03._AUST_(ILOC)_long/i,
-          'year',
-          'regionType')(filename),
-        extract(
-          /^total_(indigenous|total)_(males|females|persons)$/i,
-          'status',
-          'gender')(field)
+      R.evolve(
+        {
+          gender: v => {
+            switch (v) {
+              case 'M': return 'Males'
+              case 'F': return 'Females'
+              case 'P': return 'Persons'
+              default:  return null
+            }
+          },
+          status: v => v == 'Tot' ? 'Total' : 'Indigenous'
+        },
+        R.merge(
+          extract(
+            /(\d{4})Census_I03._AUST?_(ILOC)(?:_short)?\.csv$/i,
+            'year',
+            'regionType')(filename),
+          extract(
+            /^Tot_(Indig_|Tot)(M|F|P)$/i,
+            'status',
+            'gender')(field)
+        )
       )
     return orEmpty(
       hasKeys('year', 'regionType', 'status', 'gender'),
@@ -408,23 +466,32 @@ const matchers = [
   },
   (filename, field) => {
     const params =
-      R.merge(
-        extract(
-          /(\d{4})Census_I04_AUST_(\w+)_long/i,
-          'year',
-          'regionType')(filename),
-        extract(
-          /^(proportion_of_dwellings_that_need_1_or_more_extra_bedrooms)_(.*?indigenous|total)/i,
-          'name',
-          'status')(field)
+      R.evolve(
+        {
+          status: v =>
+            v == 'Tot' ?
+            'Total' :
+            (v.startsWith('In') ?
+              'Indigenous' :
+              'Non-Indigenous')
+        },
+        R.merge(
+          extract(
+            /(\d{4})Census_I04_AUST?_([^_\.]+)(?:_short)?\.csv$/i,
+            'year',
+            'regionType')(filename),
+          extract(
+            /^P_d_n_x_b_(In_ps_hh_w_In_p|No_In_p_oth_hh|Tot)$/i,
+            'status')(field)
+        )
       )
     return orEmpty(
-      hasKeys('year', 'regionType', 'name', 'status'),
+      hasKeys('year', 'regionType', 'status'),
       params => [{
         regionType: params.regionType.toLowerCase() + '_' + params.year,
         attribute: {
-          "name": `census${params.year}_${params.name}_${params.status}`.toLowerCase(),
-          "description": `${params.name.replace(/_/g, ' ')} - ${params.status.replace(/_/g, '-')} (Census ${params.year})`,
+          "name": `census${params.year}_proportion_of_dwellings_that_need_1_or_more_extra_bedrooms_${params.status.replace('-', '_')}`.toLowerCase(),
+          "description": `Proportion of dwellings that need 1 or more extra bedrooms - ${params.status} (Census ${params.year})`,
           "type": "number",
           "format": {
             "style": "percent"
@@ -438,13 +505,14 @@ const matchers = [
 ]
 
 // readCSV :: String → String → Promise [Object]
-const readCSV = regionColumnName => filepath => {
+const readCSV = regionColumnIndex => filepath => {
   return new Promise((resolve, reject) => {
     // TODO: make this more functional
     // (At least all the mutability is contained here.)
     const attributeRetreiver = field => {
       return R.apply(chainApplyToAll, matchers)(path.basename(filepath), field)
     }
+    let regionColumnName = null
     let attributeData = null
     const reader = fs.createReadStream(filepath)
     const parser = parseCSV({
@@ -455,6 +523,7 @@ const readCSV = regionColumnName => filepath => {
       let record;
       while (record = parser.read()) {
         if (!attributeData) {
+          regionColumnName = R.keys(record)[regionColumnIndex]
           const fields = R.reject(R.equals(regionColumnName))(R.keys(record))
           attributeData = R.chain(field => {
             const attributes = attributeRetreiver(field)
@@ -507,7 +576,7 @@ const combinedPromise = f => R.pipe(f, vs => Promise.all(vs))
 // readCSVs :: [String] → Promise [Object]
 const readCSVs =
   R.pipeP(
-    combinedPromise(R.map(readCSV('region_id'))),
+    combinedPromise(R.map(readCSV(0))),
     R.unnest)
 
 // writeAttributes :: [Object] → Promise [Object]
