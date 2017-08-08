043c5e43bbabf3bc0e4e7d904159efa22b3183ee
Tim Dettrick <t.dettrick@uq.edu.au>
2017-08-08 11:14:53 +1000

    Use faster (but less accurate) weighted mean

--- lib/util.ts
+++ lib/util.ts
@@ -1,46 +1,34 @@
-import mathjs from 'mathjs'
-
 /**
 Convert 2-ary function to take single tuple.
 */
 const tupled2: (<T, U, V>(f1: (k:T, v:U) => V) => ((t: [T, U]) => V)) =
   f => t => f(t[0], t[1])
 
-type MaybeBigNumber = number | mathjs.BigNumber
-function toBigNumber(v: MaybeBigNumber): mathjs.BigNumber {
-  return mathjs.bignumber(v as any)
-}
-function toNumber(v: MaybeBigNumber): number {
-  return mathjs.number(v as any) as number
-}
-
 class WeightedMean {
-  private readonly v: mathjs.BigNumber
-  private readonly w: mathjs.BigNumber
-  constructor(value: MaybeBigNumber, weight: MaybeBigNumber) {
-    this.v = toBigNumber(value)
-    this.w = toBigNumber(weight)
+  private readonly v: number
+  private readonly w: number
+  constructor(value: number, weight: number) {
+    this.v = value
+    this.w = weight
   }
   add(other: WeightedMean): WeightedMean {
-    const nextWeight = mathjs.add(this.w, other.w)
+    const nextWeight = this.w + other.w
     const nextValue =
-      mathjs.divide(
-        mathjs.add(
-          mathjs.multiply(this.v, this.w),
-          mathjs.multiply(other.v, other.w)
-        ),
+      (
+        (
+          (this.v * this.w) +
+          (other.v * other.w)
+        ) /
         nextWeight
       )
     return new WeightedMean(nextValue, nextWeight)
   }
   get value(): number {
-    return toNumber(this.v)
+    return this.v
   }
 }
 
 export {
   tupled2,
-  toBigNumber,
-  toNumber,
   WeightedMean
 }
--- test/lib/util.ts
+++ test/lib/util.ts
@@ -22,58 +22,79 @@ describe('util', function() {
       expect(util.WeightedMean).to.be.ok
     })
 
+    const genSensibleFiniteNumber =
+      gen.numberWithin(-1e100, 1e100)
+    const genSensiblePositiveFiniteNumber =
+      gen.numberWithin(Number.MIN_VALUE, 1e100)
+
+    /**
+     * @param a {number}
+     * @param b {number}
+     * @return absolute difference between `a` & `b` as a proportion of `a`
+     */
+    function proportionalDifference(a: number, b: number): number {
+      if (mathjs.equal(a, b)) {
+        return 0
+      } else {
+        return mathjs.number(
+          mathjs.chain(mathjs.bignumber(a))
+            .subtract(mathjs.bignumber(b))
+            .abs()
+            .divide(mathjs.abs(mathjs.bignumber(a)))
+            .done()
+        ) as number
+      }
+    }
+
     check.it(
       'operates like mean for equal weights',
-      gen.array(gen.number).notEmpty(),
+      gen.array(genSensibleFiniteNumber).notEmpty(),
       function(vs: number[]) {
-        const avg: number = util.toNumber(
-          mathjs.mean(vs.map(util.toBigNumber))
-        )
+        const avg: number =
+          mathjs.number(
+            mathjs.mean(vs.map(v => mathjs.bignumber(v)))
+          ) as number
         const output: util.WeightedMean =
           vs
             .map(v => new util.WeightedMean(v, 1))
             .reduce((a, b) => a.add(b))
-        if (isNaN(output.value)) {
-          expect(isNaN(avg)).to.be.true
-        } else {
-          expect(output.value).to.equal(avg)
-        }
+        // May not be equal, but should be proportionally extremely close
+        expect(proportionalDifference(output.value, avg))
+          .to.be.lessThan(Number.MIN_VALUE)
       })
 
       check.it(
         'handles different weights',
         gen.array(
           gen.array([
-            gen.number,
-            gen.posNumber.suchThat(R.complement(mathjs.isZero))
+            genSensibleFiniteNumber,
+            genSensiblePositiveFiniteNumber
           ], { minSize: 1 }),
           { minSize: 1 }
         ),
         function(pairs: [number, number][]) {
           const avg: number =
-            util.toNumber(
+            mathjs.number(
               mathjs.divide(
                 pairs
-                  .map(R.map(util.toBigNumber))
+                  .map(R.map(v => mathjs.bignumber(v)))
                   .map(p => mathjs.multiply(p[0], p[1]))
                   .reduce((a, b) => mathjs.add(a, b)),
                 pairs
                   .map(p => p[1])
-                  .map(util.toBigNumber)
+                  .map(v => mathjs.bignumber(v))
                   .reduce((a, b) => mathjs.add(a, b))
-              )
-            )
+              ) as number
+            ) as number
           const output: util.WeightedMean =
             pairs
               .map(util.tupled2(
                 (v: number, w: number) =>
                   new util.WeightedMean(v, w)))
               .reduce((a, b) => a.add(b))
-          if (isNaN(output.value)) {
-            expect(isNaN(avg)).to.be.true
-          } else {
-            expect(output.value).to.equal(avg)
-          }
+          // May not be equal, but should be proportionally extremely close
+          expect(proportionalDifference(output.value, avg))
+            .to.be.lessThan(Number.MIN_VALUE)
         })
   })
 
