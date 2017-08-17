c6c14283ea2f4b5aa77b8b08e408755667a4b1c1
Tim Dettrick <t.dettrick@uq.edu.au>
2017-08-17 10:32:12 +1000

    Put Travis Docker params into script

---
+++ travis_docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+exec docker run \
+  --volume $(pwd):/usr/src/app \
+  --workdir /usr/src/app \
+  --user $(id -u):$(id -g) \
+  --env HOME=$(pwd) \
+  node:${NODEJS_VERSION} \
+  -- "$@"    
